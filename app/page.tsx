b31ac6610703e63dd00c8643ac0668d8cf4fa95e
Naman Agrawal <namanagrawal1895@gmail.com>
2024-12-31 19:13:53 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-
 const HomePage = () => {
   return (
     <div>
-      <p className="text-2xl text-center">
-        This is a protected route
-      </p>
+      <p className="text-2xl text-center">This is a protected route</p>
     </div>
   );
-}
- 
+};
+
 export default HomePage;
